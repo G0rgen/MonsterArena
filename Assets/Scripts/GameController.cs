3a3988579be08b5650af5977f71fe8f5ef16afa2
Gerard Gorski <112621536+G0rgen@users.noreply.github.com>
2023-03-21 11:26:19 +0100

    21.03

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private List<Monster> monsterPrefabs;
+
     [SerializeField] private Monster monsterA;
     [SerializeField] private Monster monsterB;
 
@@ -11,6 +15,63 @@ public class GameController : MonoBehaviour
     [SerializeField] private MonsterUi monsterBUi;
 
     [SerializeField] private TextMeshProUGUI commentaryText;
+    
+    private GameInput input;
+
+    private bool isMonsterATurn = true;
+    
+    private void Awake()
+    {
+        input = new GameInput();
+
+        input.Player.Next.performed += PerformNextAction;
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void OnDestroy()
+    {
+               input.Player.Next.performed -= PerformNextAction;
+    }
+
+    private void PerformNextAction(InputAction.CallbackContext context)
+    {
+        if (monsterA.HasFainted() || monsterB.HasFainted())
+        {
+            // TODO Spawn new monster
+            return;
+        }
+        
+        Monster attacker;
+        Monster defender;
+        MonsterUi defenderUi;
+
+        if (isMonsterATurn) // Monster A greif an;
+        {
+            attacker = monsterA;
+            defender = monsterB;
+            defenderUi = monsterBUi;
+        }
+        else // Monster B greift an
+        {
+            attacker = monsterB;
+            defender = monsterA;
+            defenderUi = monsterAUi;
+        }
+        
+        attacker.Attack(defender);
+        UpdateHealth(defender, defenderUi);
+
+        isMonsterATurn = !isMonsterATurn;
+    }
 
     private void Start()
     {
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -6,11 +6,32 @@ public class Monster : MonoBehaviour
     [SerializeField] private string title = "Monster";
     [SerializeField] private float maxHealth = 10f;
 
+    [SerializeField] private string attackName = "ATTACKNNAME";
+    [SerializeField] private float attackStrengh = 2f;
+
     private float currentHealth;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+    }
+
+    public void Attack(Monster target)
+    {
+        float damage = attackStrengh;
+        // TODO Add multiplier 0.5x or 2.0x depending on elements of attacker and defender
+        target.UpdateHealth(-damage);
+    }
+
+    public bool HasFainted()
+    {
+        return currentHealth == 0;
+    }
+
+    private void UpdateHealth(float change)
+    {
+        currentHealth += change;
+        currentHealth = Mathf.Clamp(currentHealth, min: 0, maxHealth);
     }
 
     public string GetTitle()
